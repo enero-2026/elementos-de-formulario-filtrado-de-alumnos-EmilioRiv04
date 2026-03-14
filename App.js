7bbf50e6553fa5a63aa03bdd4f0a886951cefee1
RIVERA REYNA JOSE EMILIO <jose.riveraryn@uanl.edu.mx>
2026-03-14 10:29:51 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,125 @@
+/*import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});*/
+
+
+
+import React, { useState} from "react";
+import {View, Text, FlatList, StyleSheet } from "react-native";
+import {Provider as PaperProvider, Searchbar, TextInput } from "react-native-paper";
+
+export default function App() {
+  //Variable de buesqueda: 
+  const [buscaAlumno, setBuscaAlumno] = useState("");
+
+  const alumnos = [
+    { matricula: "2114354", nombre: "CANDELARIA MORA SAMANTHA" },
+    { matricula: "2121179", nombre: "AGUILAR ORTIZ LUIS ROLANDO" },
+    { matricula: "2048051", nombre: "BARRIENTOS GALLEGOS DIEGO" },
+    { matricula: "1979822", nombre: "CANO MONTIEL KELLY YISSETH" },
+    { matricula: "2111889", nombre: "CANTU SILVA JAVIER" },
+    { matricula: "2069119", nombre: "CARMONA LOZANO ANGEL EMILIANO" },
+    { matricula: "2132842", nombre: "CASTILLO ACOSTA JORGE" },
+    { matricula: "1994122", nombre: "DAVILA GONZALEZ ALDO ADRIAN" },
+    { matricula: "2018230", nombre: "DURAN BARRIENTOS FABRIZIO" },
+    { matricula: "2104564", nombre: "FLORES GONZALEZ SEBASTIAN" },
+    { matricula: "2066033", nombre: "FLORES LÓPEZ DIEGO" },
+    { matricula: "2132976", nombre: "FLORES MARTINEZ ERICK ADRIAN" },
+    { matricula: "2066114", nombre: "GARZA AVALOS DIEGO" },
+    { matricula: "2031243", nombre: "GONZALEZ OVALLE CHRISTIAN GABRIEL" },
+    { matricula: "2064733", nombre: "GRANJA PEÑA DIEGO" },
+    { matricula: "2094647", nombre: "IBARRA RODRIGUEZ ALEXIS" },
+    { matricula: "2005102", nombre: "MARTINEZ ELIAS ANGEL SEBASTIAN" },
+    { matricula: "2064574", nombre: "MENDIETA GONZALEZ ESMERALDA GABRIELA" },
+    { matricula: "2024783", nombre: "MIRELES VELAZQUEZ ALEJANDRO" },
+    { matricula: "2066077", nombre: "MONSIVAIS SALAZAR ANDRES" },
+    { matricula: "2092151", nombre: "PARRAZALEZ VALDESPINO MARTHA JULIETA" },
+    { matricula: "2103708", nombre: "PEÑA MUNGARRO LUIS ANGEL" },
+    { matricula: "2115192", nombre: "PUENTE REYNOSO JULIO CESAR" },
+    { matricula: "2103765", nombre: "RAMIREZ LOPEZ BRYAN" },
+    { matricula: "2056778", nombre: "RAMOS AVILA LILIANA VALERIA" },
+    { matricula: "2037503", nombre: "RICO JAUREGUI MAURICIO" },
+    { matricula: "2131513", nombre: "RIVERA LUNA ADRIAN" },
+    { matricula: "2013503", nombre: "RIVERA REYNA JOSE EMILIO" },
+    { matricula: "2004613", nombre: "RODRIGUEZ OLVERA ROSA ISELA" },
+    { matricula: "2133022", nombre: "RODRIGUEZ RODRIGUEZ ANGEL AZAEL" },
+    { matricula: "2026061", nombre: "SANCHEZ GALARZA JUAN CARLOS" },
+    { matricula: "2095320", nombre: "SOLIS ORTIZ ALFREDO" },
+    { matricula: "2025350", nombre: "VELAZQUEZ ABREGO HERWIN DANIEL" },
+    { matricula: "2103895", nombre: "VILLAGRA RODRIGUEZ ANDRES NEHUEL" },
+    { matricula: "1857791", nombre: "ZACATENCO OLIVE RODRIGO" },
+    { matricula: "2025218", nombre: "ZAVALA CANTU TERESA MARGARITA" }
+  ];
+
+  //filtro .filter() 
+  const alumnosFiltrados = alumnos.filter((alumno) =>
+    alumno.nombre.toLowerCase().includes(buscaAlumno.toLowerCase())  ||
+    alumno.matricula.includes(buscaAlumno)
+  );
+   return (
+    <PaperProvider>
+      <View style={styles.container}>
+
+        <TextInput
+          label="Buscar Alumno"
+          mode="flat"
+          value = {buscaAlumno}
+          onChangeText={setBuscaAlumno}
+          style={styles.input}
+        />
+        <FlatList
+          data={alumnosFiltrados}
+          keyExtractor={(item) => item.matricula}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={styles.matricula}>{item.matricula}</Text>
+              <Text style={styles.nombre}>{item.nombre}</Text>
+            </View>
+          )}
+        />
+
+      </View>
+    </PaperProvider>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    marginTop: 40
+  },
+  search: {
+    marginBottom: 20
+  },
+  card: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: "#ccc"
+  },
+  matricula: {
+    fontWeight: "bold"
+  },
+  nombre: {
+    fontSize: 16
+  }
+});
+
+  
+
